18b25325ccc970c841dc692a72c852e6d687d0ae
Felipe de Lima Passarelli <malamdorox@gmail.com>
2025-12-20 11:57:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //API Key
-const apiKey = "4d041490c2bf43de91e173402250112";
+const apiKey = "d7367f3bbed21c0a06d522b0eb892f59";
 
 //form
 const form = document.getElementById("form");
@@ -81,4 +81,5 @@ const defaultCity = async () => {
 };
 
 defaultCity();
+
 
